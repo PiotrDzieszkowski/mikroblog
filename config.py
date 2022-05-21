f74c1447ea1d273bae82f6e2e1fca185ee067eb0
Piotr Dzieszkowski <piotr.dzieszkowski@gmail.com>
2022-05-21 19:23:54 +0100

    mikrblog

--- config.py
+++ config.py
@@ -3,7 +3,13 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-   SECRET_KEY = os.environ.get("mlody")
+   APP_NAME = os.environ.get('APP_NAME', 'Flask-Base')
+   if os.environ.get('SECRET_KEY'):
+        SECRET_KEY = os.environ.get('SECRET_KEY')
+   else:
+        SECRET_KEY = 'SECRET_KEY_ENV_VAR_NOT_SET'
+        print('SECRET KEY ENV VAR NOT SET! SHOULD NOT SEE IN PRODUCTION')
+   #SECRET_KEY = os.environ.get("mlody")
    SQLALCHEMY_DATABASE_URI = ( 
            os.environ.get('DATABASE_URL') or
            'sqlite:///' + os.path.join(BASE_DIR, 'library.db')
